d33f68b6337181a9c0215ba9254dc76c59fac3ca
pee47kay <papakowdadson@gmail.com>
2022-07-08 14:17:34 +0000

    added splash screen delay

--- g4app/lib/main.dart
+++ g4app/lib/main.dart
@@ -1,7 +1,7 @@
 // import 'dart:js';
 
 import 'package:flutter/material.dart';
-// import 'package:g4app/pages/login.dart';
+import 'package:g4app/pages/login.dart';
 import 'package:g4app/pages/notifications.dart';
 import 'package:g4app/pages/signup.dart';
 import 'package:g4app/pages/splash.dart';
@@ -11,10 +11,10 @@ void main() {
     theme: ThemeData(
 scaffoldBackgroundColor: Colors.white),
     // home: splash(),
-    initialRoute: '/signup',
+    // initialRoute: '/login',
     routes: {
       '/' : (context) => splash(),
-      // '/login' : (context) => login(),
+      '/login' : (context) => login(),
       '/signup' : (context) => signup(),
       '/notification' : (context) => notifications(),
     },
--- g4app/lib/pages/splash.dart
+++ g4app/lib/pages/splash.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:g4app/pages/login.dart';
 // import 'package:http/http.dart';
 
 class splash extends StatefulWidget {
-  const splash({ Key? key }) : super(key: key);
+  const splash({Key? key}) : super(key: key);
 
   @override
   State<splash> createState() => _splashState();
@@ -10,47 +11,68 @@ class splash extends StatefulWidget {
 
 class _splashState extends State<splash> {
   @override
+  void initState() {
+    //after 3 seconds load login page
+    super.initState();
+    Future.delayed(
+      const Duration(seconds: 5),
+      () {
+        Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => login(),
+            ));
+      },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green[400],
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(6.0,3.0, 6.0,0.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              
-              child:ClipRRect(
-                borderRadius:BorderRadius.circular(200),
-                child:Image.asset('assets/logo.png',
-                width: 170,
-                height: 170,),
-              
-              ),
-
-          ),
-          SizedBox(height: 100,),
-          Container(
-            child:Text('G4 App',
-                style: TextStyle(fontWeight: FontWeight.bold,
-                fontSize: 30),
-            ),
-
-          ),
-          SizedBox(height: 10,),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
+      // backgroundColor: Colors.green[400],
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(12.0, 3.0, 12.0, 10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                child: Text("Welcome to Ghana's Premium Intelligence road classification App"
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(200),
+                      child: Image.asset(
+                        'assets/logo.png',
+                        width: 170,
+                        height: 170,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      'G4 App',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                    ),
+                  ],
                 ),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    child: Text(
+                      "Welcome to Ghana's Premium Intelligence road classification App",
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
-          ],
         ),
       ),
-      
     );
   }
 }

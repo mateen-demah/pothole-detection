f51a55358bf587865d21e9241484c9be9de2b8cd
Abdul-Mateen <saeedabdulmateen0369@gmail.com>
2022-09-27 04:18:22 +0000

    data collection app made a standalone flutter project

---
+++ data_app/lib/data_history.dart
@@ -0,0 +1,352 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:sqflite_common_porter/utils/csv_utils.dart';
+
+import 'collection_page.dart';
+import 'database.dart';
+
+class DataHistoryPage extends StatefulWidget {
+  @override
+  State<DataHistoryPage> createState() => _DataHistoryPageState();
+}
+
+class _DataHistoryPageState extends State<DataHistoryPage> {
+  List<Map<String, Object?>>? dataSessions;
+  final _newSessionIdController = TextEditingController();
+  String? _newSessionMode;
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    readSessionInfo();
+    super.initState();
+  }
+
+  Future<void> readSessionInfo() async {
+    dataSessions = await SensorDataDb.instance.readAllSessionInfo();
+    final db = await SensorDataDb.instance.database;
+    final data =
+        await db!.rawQuery("SELECT * FROM sqlite_master WHERE type='table';");
+    print('this run');
+    // print(data);
+    data.toList().forEach(
+          (element) => print(element),
+        );
+    setState(() {});
+  }
+
+  void snackbar(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        // backgroundColor: Colors.green,
+        elevation: 10,
+        behavior: SnackBarBehavior.floating,
+        margin: EdgeInsets.only(left: 5, bottom: 8, right: 105),
+        duration: Duration(milliseconds: 500),
+      ),
+    );
+  }
+
+  void dialog(String error, BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              title: Text('error'),
+              content: Text(error),
+            ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Data History")),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: dataSessions == null
+            ? Center(child: CircularProgressIndicator())
+            : Column(
+                children: [
+                  Expanded(
+                    child: dataSessions!.length == 0
+                        ? Center(
+                            child: Text(
+                                "Data collection sessions would show up here"),
+                          )
+                        : ListView.builder(
+                            itemCount: dataSessions!.length,
+                            itemBuilder: (context, index) => ListTile(
+                              title: Text(dataSessions![index]["session_id"]
+                                  .toString()),
+                              trailing: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  IconButton(
+                                      onPressed: () async {
+                                        final db = await SensorDataDb
+                                            .instance.database;
+                                        // final result = await db?.query(
+                                        //     'rawSensorData',
+                                        //     where: "session_id = ?",
+                                        //     whereArgs: [
+                                        //       dataSessions![index]['session_id']
+                                        //     ]);
+                                        final sensorData = await db?.rawQuery(
+                                            "SELECT * FROM rawSensorData where session_id = ?",
+                                            [
+                                              dataSessions![index]['session_id']
+                                            ]);
+                                        final potholes = await db?.rawQuery(
+                                            "SELECT * FROM potholes where session_id = ?",
+                                            [
+                                              dataSessions![index]['session_id']
+                                            ]);
+                                        final ramps = await db?.rawQuery(
+                                            "SELECT * FROM ramps where session_id = ?",
+                                            [
+                                              dataSessions![index]['session_id']
+                                            ]);
+                                        final sensorData_csv =
+                                            mapListToCsv(sensorData);
+                                        final potholes_csv =
+                                            mapListToCsv(potholes);
+                                        final ramps_csv = mapListToCsv(ramps);
+                                        // print(
+                                        //     '>>>>>>>>>>>> CSV <<<<<<<<<<<<<<<<');
+                                        // print(result);
+                                        String downloads =
+                                            '/storage/emulated/0/Download';
+
+                                        File sf = File(downloads +
+                                            "/${dataSessions![index]['session_id']}_sensordata.csv");
+                                        File pf = File(downloads +
+                                            "/${dataSessions![index]['session_id']}_potholes.csv");
+                                        File rf = File(downloads +
+                                            "/${dataSessions![index]['session_id']}_ramps.csv");
+
+                                        try {
+                                          final skip = await sf.exists();
+                                          if (!skip)
+                                            sf.writeAsString(sensorData_csv!);
+                                          // sf.delete();
+                                          snackbar("sensor data saved");
+                                        } catch (e) {
+                                          snackbar(
+                                              "failed to save sensor data");
+                                          dialog(e.toString(), context);
+                                        }
+                                        try {
+                                          final skipp = await pf.exists();
+                                          if (!skipp)
+                                            pf.writeAsString(potholes_csv!);
+                                          // pf.delete();
+                                          snackbar("pothole data saved");
+                                        } catch (e) {
+                                          snackbar(
+                                              "failed to save pothole data");
+                                          dialog(e.toString(), context);
+                                        }
+                                        try {
+                                          final skipp = await rf.exists();
+                                          if (!skipp)
+                                            rf.writeAsString(ramps_csv!);
+                                          // pf.delete();
+                                          snackbar("ramp data saved");
+                                        } catch (e) {
+                                          snackbar("failed to save ramp data");
+                                          dialog(e.toString(), context);
+                                        }
+                                      },
+                                      icon: Icon(Icons.import_export)),
+                                  IconButton(
+                                    icon: Icon(
+                                      Icons.delete,
+                                      color: Colors.red,
+                                    ),
+                                    onPressed: () => showDialog(
+                                        context: context,
+                                        builder: (context) => AlertDialog(
+                                              title: Text("Are you Sure?"),
+                                              content: Text(
+                                                  "this action is irreversible"),
+                                              actions: [
+                                                TextButton(
+                                                  onPressed: () =>
+                                                      deleteSessionData(
+                                                          index: index),
+                                                  child: Text("yes"),
+                                                  style: ButtonStyle(
+                                                    backgroundColor:
+                                                        MaterialStateProperty
+                                                            .all(Colors.red),
+                                                    foregroundColor:
+                                                        MaterialStateProperty
+                                                            .all(Colors.white),
+                                                  ),
+                                                ),
+                                                TextButton(
+                                                  onPressed: () =>
+                                                      Navigator.of(context)
+                                                          .pop(),
+                                                  child: Text("no"),
+                                                  style: ButtonStyle(
+                                                    backgroundColor:
+                                                        MaterialStateProperty
+                                                            .all(Colors.green),
+                                                    foregroundColor:
+                                                        MaterialStateProperty
+                                                            .all(Colors.white),
+                                                  ),
+                                                )
+                                              ],
+                                            )),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            // Text(dataSessions![index]["session_id"].toString()),
+                          ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: ElevatedButton(
+                      child: Text("New Data"),
+                      onPressed: showNewDataDialog,
+                    ),
+                  )
+                ],
+              ),
+      ),
+    );
+  }
+
+  void showNewDataDialog() {
+    showModalBottomSheet(
+      isScrollControlled: true,
+      elevation: 6,
+      //barrierColor: Theme.of(context).accentColor,
+      context: context,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      builder: (BuildContext context) {
+        return SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.only(
+              bottom: MediaQuery.of(context).viewInsets.bottom,
+              top: 8,
+              left: 8,
+              right: 8,
+            ),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  DropdownButtonFormField(
+                    items: <DropdownMenuItem<String>>[
+                      DropdownMenuItem(
+                        child: Text('potholes/ramps'),
+                        value: "potholes",
+                      ),
+                      DropdownMenuItem(
+                        child: Text('road categorisation'),
+                        value: 'road categorisation',
+                      ),
+                    ],
+                    onChanged: (value) {
+                      _newSessionMode = value as String?;
+                    },
+                    hint: Text("select mode"),
+                    validator: (value) {
+                      if (value == null) return "session mode is required";
+                      return null;
+                    },
+                  ),
+                  Row(
+                    children: [
+                      Text("session ID: "),
+                      Container(
+                        margin: EdgeInsets.all(8),
+                        width: MediaQuery.of(context).size.width - 120,
+                        child: TextFormField(
+                          controller: _newSessionIdController,
+                          decoration: InputDecoration(
+                            hintText: "identifier for new session",
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty)
+                              return "Session ID can't be empty";
+                            if (dataSessions!.any((element) {
+                              print(
+                                  '${element['session_id'].toString()}$value');
+                              return element["session_id"].toString == value;
+                            })) return "ID already exist";
+                            return null;
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: ElevatedButton(
+                      child: Text("create session"),
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          Navigator.of(context).pop();
+                          final sessionWasSuccessful =
+                              await Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => CollectionPage(
+                                  sessionID: _newSessionIdController.text,
+                                  sessionMode: _newSessionMode),
+                            ),
+                          );
+                          if (sessionWasSuccessful)
+                            readSessionInfo();
+                          else
+                            deleteSessionData(
+                                sessionID: _newSessionIdController.text);
+                          _newSessionIdController.text = '';
+                        }
+                      },
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  void deleteSessionData({int? index, String? sessionID}) async {
+    final _sessionID = index == null
+        ? sessionID
+        : dataSessions![index]['session_id'].toString();
+    final success = await SensorDataDb.instance.deleteSession(_sessionID!);
+    print(success);
+    if (success[2] > 0) {
+      if (index != null) {
+        Navigator.of(context).pop();
+        setState(() {
+          dataSessions!.removeAt(index);
+        });
+      }
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            index != null
+                ? 'data deleted successfully'
+                : 'data from session has been discarded',
+            textAlign: TextAlign.center,
+          ),
+          // backgroundColor: Colors.green,
+          elevation: 10,
+          behavior: SnackBarBehavior.floating,
+          margin: EdgeInsets.only(left: 5, bottom: 8, right: 105),
+        ),
+      );
+    }
+  }
+}
---
+++ data_app/lib/database.dart
@@ -0,0 +1,157 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class SensorDataDb {
+  static final SensorDataDb instance = SensorDataDb.init();
+  static Database? sensorDataDb;
+
+  SensorDataDb.init();
+
+  Future<Database?> get database async {
+    // if (sensorDataDb != null) return sensorDataDb;
+    sensorDataDb = await initialiseDb('sensorData.db');
+    return sensorDataDb;
+  }
+
+  Future<Database> initialiseDb(String dbFileName) async {
+    final dir = await getDatabasesPath();
+    final dbPath = join(dir, dbFileName);
+    return await openDatabase(
+      dbPath,
+      version: 1,
+      onCreate: createTables,
+      singleInstance: true,
+    );
+  }
+
+  Future createTables(Database db, int version) async {
+    // schema for raw sensor data
+    await db.execute('''CREATE TABLE rawSensorData (
+      timestamp INTEGER PRIMARY KEY,
+      latitude REAL NOT NULL,
+      longitude REAL NOT NULL,
+      accelerometer_x REAL NOT NULL,
+      accelerometer_y REAL NOT NULL,
+      accelerometer_z REAL NOT NULL,
+      gyroscope_x REAL NOT NULL,
+      gyroscope_y REAL NOT NULL,
+      gyroscope_z REAL NOT NULL,
+      moving_speed REAL NOT NULL,
+      moving_speed_accuracy REAL NOT NULL,
+      session_id TEXT NOT NULL,
+      is_extracted INTEGER DEFAULT 0
+      )''');
+
+    // schema for registered potholes
+    await db.execute('''CREATE TABLE potholes (
+      timestamp INTEGER PRIMARY KEY,
+      session_id TEXT NOT NULL,
+      is_exported INTEGER DEFAULT 0
+    )''');
+
+    await db.execute('''CREATE TABLE ramps (
+      timestamp INTEGER PRIMARY KEY,
+      session_id TEXT NOT NULL,
+      is_exported INTEGER DEFAULT 0
+    )''');
+
+    // schema for sessionInfo
+    await db.execute('''CREATE TABLE sessionInfo(
+      session_id TEXT PRIMARY KEY,
+      is_extracted INTEGER DEFAULT 0,
+      is_predicted INTEGER DEFAULT 0,
+      is_uploaded INTEGER DEFAULT 0,
+      location_corrected INTEGER DEFAULT 0,
+      date INTEGER
+    )''');
+
+    // schema road class labelling data
+    await db.execute('''CREATE TABLE rawRoadSegmentLabel(
+      timestamp INTEGER PRIMARY KEY,
+      session_id TEXT NOT NULL,
+      is_exported INTEGER DEFAULT 0,
+      category TEXT NOT NULL
+    )''');
+
+    await db.execute('''CREATE TABLE extractedFeatures (
+      session_id TEXT NOT NULL,
+      timestamp INTEGER PRIMARY KEY,
+      latitude REAL NOT NULL,
+      longitude REAL NOT NULL,
+      avg_acc_x REAL NOT NULL,
+      avg_acc_y REAL NOT NULL,
+      avg_acc_z REAL NOT NULL,
+      std_acc_x REAL NOT NULL,
+      std_acc_y REAL NOT NULL,
+      std_acc_z REAL NOT NULL,
+      max_acc_x REAL NOT NULL,
+      max_acc_y REAL NOT NULL,
+      max_acc_z REAL NOT NULL,
+      min_acc_x REAL NOT NULL,
+      min_acc_y REAL NOT NULL,
+      min_acc_z REAL NOT NULL,
+      avg_gyro_x REAL NOT NULL,
+      avg_gyro_y REAL NOT NULL,
+      avg_gyro_z REAL NOT NULL,
+      std_gyro_x REAL NOT NULL,
+      std_gyro_y REAL NOT NULL,
+      std_gyro_z REAL NOT NULL,
+      max_gyro_x REAL NOT NULL,
+      max_gyro_y REAL NOT NULL,
+      max_gyro_z REAL NOT NULL,
+      min_gyro_x REAL NOT NULL,
+      min_gyro_y REAL NOT NULL,
+      min_gyro_z REAL NOT NULL,
+      avg_speed REAL NOT NULL,
+      std_speed REAL NOT NULL,
+      max_speed REAL NOT NULL,
+      min_speed REAL NOT NULL,
+      label INTEGER DEFAULT 0,
+      geohash REAL
+      )''');
+  }
+
+  Future<int> insert(String tableName, Map<String, Object> sensorData) async {
+    final db = await instance.database;
+    final id = await db!.insert(
+      tableName,
+      sensorData,
+    );
+    return id;
+  }
+
+  Future<List<int>> deleteSession(String session_id) async {
+    final db = await instance.database;
+    final deletedRaw = await db!.delete(
+      'rawSensorData',
+      where: 'session_id = ?',
+      whereArgs: [session_id],
+    );
+    final deletedPotholes = await db.delete(
+      'potholes',
+      where: 'session_id = ?',
+      whereArgs: [session_id],
+    );
+    final deletedRamps = await db.delete(
+      'ramps',
+      where: 'session_id = ?',
+      whereArgs: [session_id],
+    );
+    final deletedSession = await db.delete('sessionInfo',
+        where: 'session_id = ?', whereArgs: [session_id]);
+    print(
+        "$deletedRaw ===========\n$deletedPotholes =================\n$deletedSession =========================");
+    return [deletedRaw, deletedPotholes, deletedSession];
+  }
+
+  Future<List<Map<String, Object?>>> readAllSessionInfo() async {
+    final db = await instance.database;
+    final sessionInfoList = await db!.query("sessionInfo", orderBy: "date");
+    return sessionInfoList.toList();
+  }
+
+  Future<void> close() async {
+    final db = await instance.database;
+    db!.close();
+  }
+}
